b0f96d1f4da7978e55f58268ba9fd34d335d3195
Sourav Prem <sourav.prem@im2258-x1.corp.inmobi.com>
2018-03-20 11:31:53 +0530

    Add .idea to .gitignore and remove unnecessary form

--- imports/ui/views/AdomainForm.js
+++ imports/ui/views/AdomainForm.js
@@ -105,22 +105,6 @@ class AdomainForm extends Component {
                                         </form>
                                 </div>
                             </div>
-
-
-
-
-                            <form >
-                                <label htmlFor="username">Enter username</label>
-                                <input id="username" name="username" type="text"/>
-
-                                <label htmlFor="email">Enter your email</label>
-                                <input id="email" name="email" type="email"/>
-
-                                <label htmlFor="birthdate">Enter your birth date</label>
-                                <input id="birthdate" name="birthdate" type="text"/>
-
-                                <button>Send data!</button>
-                            </form>
                         </div>
                     </div>
                 </div>
